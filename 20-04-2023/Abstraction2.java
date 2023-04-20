b6ad0a74c8ff4044dae41970853dad8492b47701
sakshimirkar <78417855+sakshimirikar@users.noreply.github.com>
2023-04-20 20:57:25 +0530

    Update Abstraction2.java

--- 20-04-2023/Abstraction2.java
+++ 20-04-2023/Abstraction2.java
@@ -1,62 +1,81 @@
 
+
 package Lab;
 
-abstract class BankAccount{
+import java.util.Scanner;
+
+
+abstract class BankAccount
+{
+	int acc_number, balance;
 	
-	int account_number;
-	double balance;
-	
-	abstract double deposit();
-	abstract double withdraw();
-	
+	public BankAccount(int acc_number, int balance)
+	{
+		this.acc_number = acc_number;
+		this.balance = balance;
+	}
+	abstract void deposit(int amount);
+	abstract void withdraw(int amount);
 }
 
-class CheckingAccount extends BankAccount{
-	
-   double balance;
-	double deposit;
-	double withdraw;
-	
+class CheckingAccount extends BankAccount
+{
 
-	public CheckingAccount(double balance , double deposit , double withdraw) {
-
-		this.balance=balance;
-		this.deposit=deposit;
-		this.withdraw=withdraw;
-		
+	public CheckingAccount(int acc_number, int balance) {
+		super(acc_number, balance);
 	}
 
 	@Override
-	double deposit() {
-		
-		deposit=balance+deposit;
-		return deposit;	
-		
+	void deposit(int deposit_amount) {
+		System.out.println("Balance after deposit: "+(balance+deposit_amount));
 	}
 
 	@Override
-	double withdraw() {
-		
-		withdraw=balance - withdraw;
-		return withdraw;	
-		
+	void withdraw(int withdraw_amount) {
+		if (withdraw_amount<=balance)
+		{
+			System.out.println("Withdraw successfully!!");
+			System.out.println("Current balance : "+(balance-withdraw_amount));
+		}
+		else
+		{
+			System.out.println("Insufficient balance");
+		}
 	}
 	
-	
 }
-
 public class Abstraction2 {
 
 	public static void main(String[] args) {
-		
-		
-		CheckingAccount acc1 = new CheckingAccount(1000 , 500 , 300);
-		System.out.println("Deposit : " + acc1.deposit());
-		System.out.println("Withdraw : " + acc1.withdraw());
-		
-		
-		
-		
+		Scanner sc = new Scanner(System.in);
+		CheckingAccount ca = new CheckingAccount(123465, 10000);
+		do {
+			System.out.println("1)Deposit 2)Withdraw  3)Exit");
+			int choice = sc.nextInt();
+			switch(choice)
+			{
+			case 1:
+				System.out.println("Enter the amount you want to deposit: ");
+				int deposit_amount = sc.nextInt();
+				ca.deposit(deposit_amount);
+				break;
+				
+			case 2:
+				System.out.println("Enter the amount you want to Withdraw: ");
+				int withdraw_amount = sc.nextInt();
+				ca.withdraw(withdraw_amount);
+				break;
+				
+			case 3:
+				System.out.println("Thank you for visiting !");
+				System.exit(0);
+				break;
+				
+			default:
+				System.out.println("Invalid choice!!");
+				break;
+			}
+		}while(true);
 
 	}
 
